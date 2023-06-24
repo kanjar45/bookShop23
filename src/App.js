c29a067bae7729dd12496d0e497db931166a2ea4
kubanychhbekova <a7966519@gmail.com>
2023-06-24 13:02:50 +0600

    finish second page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Footer from "./components/footer/footer";
 import Pages from "./components/pages/pages";
 import Header from "./components/header/header";
 import Cart from "./components/myCart/Cart";
-import {Route, Routes} from "react-router";
 import {Route, Routes} from "react-router-dom";
 import AllBooks from "./components/allBooks/AllBooks";
+import DetailPage from "./components/datailPage/datailPage";
 
 function App() {
   return (
@@ -14,11 +14,10 @@ function App() {
         <Routes>
             <Route path={'/'} element={<Pages/>}/>
             <Route path={'/cart'} element={<Cart/>}/>
-        </Routes>
-            <Route path={"/"} element={<Pages/>}/>
                 <Route path={"/books"} element={<AllBooks/>}/>
-
+                <Route path={"/books/:id"} element={<DetailPage/>}/>
         </Routes>
+
 
     <Footer/>
     </div>
--- src/components/allBooks/AllBooks.js
+++ src/components/allBooks/AllBooks.js
@@ -1,79 +1,11 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import Data from "../data";
+import {NavLink, useParams} from "react-router-dom";
 const AllBooks = () => {
-    const Data = [
-        {
-            id: 1,
-            title: "MongoDB in Action, 2nd Edition",
-            subtitle: "Covers MongoDB version 3.0",
-            price: 20,
-            image: "https://itbook.store/img/books/9781617291609.png"
-        },
-        {
-            id: 2,
-            title: "MongoDB and Python",
-            subtitle: "Patterns and processes for the popular document-oriented database",
-            price: 7,
-            image: "https://itbook.store/img/books/9781449310370.png",
-        },
-        {
-            id: 3,
-            title: "Building Node Applications with MongoDB and Backbone",
-            subtitle: "Rapid Prototyping and Scalable Deployment",
-            price: 5,
-            image: "https://itbook.store/img/books/9781449337391.png",
-            url: "https://itbook.store/books/9781449337391"
-        },
-        {
-            id: 4,
-            title: "Practical MongoDB",
-            subtitle: "Architecting, Developing, and Administering MongoDB",
-            price: 41,
-            image: "https://itbook.store/img/books/9781484206485.png",
-        },
-        {
-            id: 5,
-            title: "The Definitive Guide to MongoDB, 3rd Edition",
-            subtitle: "A complete guide to dealing with Big Data using MongoDB",
-            price: 50,
-            image: "https://itbook.store/img/books/9781484211830.png",
-        },
-        {
-            id: 6,
-            title: "MongoDB Performance Tuning",
-            subtitle: "Optimizing MongoDB Databases and their Applications",
-            price: 34,
-            image: "https://itbook.store/img/books/9781484268780.png",
-        },
-        {
-            id: 7,
-            title: "Pentaho Analytics for MongoDB",
-            subtitle: "Combine Pentaho Analytics and MongoDB to create powerful analysis and reporting solutions",
-            price: 17,
-            image: "https://itbook.store/img/books/9781782168355.png",
-        },
-        {
-            id: 8,
-            title: "Pentaho Analytics for MongoDB Cookbook",
-            subtitle: "Over 50 recipes to learn how to use Pentaho Analytics and MongoDB to create powerful analysis and reporting solutions",
-            price: 44,
-            image: "https://itbook.store/img/books/9781783553273.png",
-        },
-        {
-            id: 9,
-            title: "Web Development with MongoDB and NodeJS, 2nd Edition",
-            subtitle: "Build an interactive and full-featured web application from scratch using Node.js and MongoDB",
-            price: 39,
-            image: "https://itbook.store/img/books/9781785287527.png",
-        },
-        {
-            id: 10,
-            title: "MongoDB Cookbook, 2nd Edition",
-            subtitle: "Harness the latest features of MongoDB 3 with this collection of 80 recipes - from managing cloud platforms to app development, this book is a vital resource",
-            price: 44.99,
-            image: "https://itbook.store/img/books/9781785289989.png",
-        }
-    ]
+const [books,setBooks]=useState([])
+    useEffect(()=>{
+        setBooks(Data)
+    },[])
     return (
         <div id="books">
             <div className="container">
@@ -163,14 +95,17 @@ const AllBooks = () => {
 
                         </div>
                         <div className="books--bottom__right ">
-                            {Data.map((el) => {
+                            {
+                                books.map((el) => {
                                 return (
                                     <div key={el.id} className="books--bottom__right--card">
-                                        <img style={{
-                                            width: "285px",
-                                            height: "310px",
+                                      <NavLink key={el.id} to={`/books/${el.id}`}>
+                                          <img style={{
+                                              width: "285px",
+                                              height: "310px",
 
-                                        }} src={el.image} alt=""/>
+                                          }} src={el.image} alt=""/>
+                                      </NavLink>
                                         <div>
                                             <h5>{el.title}</h5>
                                         </div>

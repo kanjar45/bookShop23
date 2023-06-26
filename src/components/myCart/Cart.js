80a351b0ee9b8ff1e03fb15f7ee3c398e12c753d
nursdeveloper <amanturovnursultan458@gmail.com>
2023-06-27 01:36:59 +0300

    cart

--- src/components/myCart/Cart.js
+++ src/components/myCart/Cart.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import './style.scss.css'
+import {IoIosArrowForward} from "react-icons/io";
 const Cart = () => {
     return (
-        <div className='container'>
-            <div className="caption">
-                <h1>Your cart</h1>
-                <h4>Not ready to checkout? <span style={{color:'skyblue'}}>Continue Shopping</span> </h4>
-            </div>
-            <div className="cart-content">
-                <div className='image'></div>
-                <div className="res">
-                    <h1>Order Summary</h1>
-                    <div className="content">
-                        <div className="shipping">
-                            <p>Shipping</p>
-                        <p>Payment</p>
-                        <p>total</p>
-                        </div>
-                        <div className="select">
-                            <h5>Select Method</h5>
-                            <h5>Select Method</h5>
-                        </div>
-                        <div className='linia'></div> 
-                    </div>
-                    <button>Continue to checkout</button>
+        <div className='cart'>
+            <div className="yourCart">
+                <div className="zagalovok">
+                    <h1>Your cart</h1>
+                    <p>Not ready to checkout? <a href="http://localhost:3000/">Continue Shopping </a></p>
+                    <div></div>
                 </div>
             </div>
-
+            <div className="cart_content">
+                <h1>Order Summary</h1>
+                <div className="order">
+                    <div className="shipping">
+                        <h5>Shipping</h5>
+                        <h5>Select Method <IoIosArrowForward/></h5>
+                    </div>
+                    <div className="Payment">
+                        <h5>Payment</h5>
+                        <h5>Select Method <IoIosArrowForward/></h5>
+                    </div>
+                    <div className='linear'></div>
+                    <div className="total">
+                        <h5>Total</h5>
+                        <p>$</p>
+                    </div>
+                </div>
+                <button>Continue to checkout</button>
+            </div>
         </div>
     );
 };

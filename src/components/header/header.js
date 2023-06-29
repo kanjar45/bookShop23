cce8a12de06a07ff0db0898fca6de807d2e044fa
kubanychhbekova <a7966519@gmail.com>
2023-06-29 12:52:45 +0600

    burger

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,39 +1,49 @@
 import React from 'react';
-import {NavLink,Link} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 
 import {BiSearch} from "react-icons/bi";
 import {HiOutlineShoppingBag} from "react-icons/hi";
+
 const Header = () => {
     return (
         <div id="header">
             <div className="container">
-                    <div className="header">
-                        <ul className="header--ul" >
-                            <li className="header--ul__li">
-                                <NavLink to={"/"} className="header--ul__li--title " >Bookshop</NavLink>
-                            </li>
-                            <li className="header--ul__li">
-                                <NavLink to={"/books"}   className="header--ul__li--title" >Categories</NavLink>
-                            </li>
-                            <li className="header--ul__li">
-                                <NavLink to={"/recent"}   className="header--ul__li--title" >Recent</NavLink>
-                            </li>
-                            <li className="header--ul__li">
-                                <NavLink to={"/hello"}   className="header--ul__li--title" >Books</NavLink>
-                            </li>
-                            <li className="header--ul__li">
-                                <NavLink to={"/about"}   className="header--ul__li--title" >About Us</NavLink>
-                            </li>
+                <div className="header">
+                    <ul className="header--ul">
+                        <li className="header--ul__li">
+                            <NavLink to={"/"} className="header--ul__li--title ">Bookshop</NavLink>
+                        </li>
+                        <li className="header--ul__li">
+                            <NavLink to={"/books"} className="header--ul__li--title">Categories</NavLink>
+                        </li>
+                        <li className="header--ul__li">
+                            <NavLink to={"/recent"} className="header--ul__li--title">Recent</NavLink>
+                        </li>
+                        <li className="header--ul__li">
+                            <NavLink to={"/hello"} className="header--ul__li--title">Books</NavLink>
+                        </li>
+                        <li className="header--ul__li">
+                            <NavLink to={"/about"} className="header--ul__li--title">About Us</NavLink>
+                        </li>
 
 
-                        </ul>
-                        <div className="header--icons" >
-                            <BiSearch className="header--icons__search "/>
-                            <Link to={'/cart'}>
-                                <HiOutlineShoppingBag  className="header--icons__basket"/>
-                            </Link>
+                    </ul>
+                    <div className="header--icons">
+                        <BiSearch className="header--icons__search "/>
+                        <Link to={'/cart'}>
+                            <HiOutlineShoppingBag className="header--icons__basket"/>
+                        </Link>
+                        <div className="header--icons__burger">
+                            <button className="header--icons__burger--btn">
+                                   <span></span>
+                                   <span></span>
+                                   <span></span>
+
+                            </button>
                         </div>
                     </div>
+
+                </div>
 
 
             </div>

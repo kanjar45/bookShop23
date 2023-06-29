4427151b8b21d1eeb14f44fbab90ddfe035d54df
kubanychhbekova <a7966519@gmail.com>
2023-06-29 14:39:14 +0600

    finish burger

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink, Link} from "react-router-dom";
 
 import {BiSearch} from "react-icons/bi";
 import {HiOutlineShoppingBag} from "react-icons/hi";
+import transitionEndListener from "react-bootstrap/transitionEndListener";
 
 const Header = () => {
+    const [burger,setBurger]=useState(false)
     return (
         <div id="header">
             <div className="container">
-                <div className="header">
+                <div className={`header ${burger? "open" : ""} `}>
                     <ul className="header--ul">
                         <li className="header--ul__li">
                             <NavLink to={"/"} className="header--ul__li--title ">Bookshop</NavLink>
                         </li>
-                        <li className="header--ul__li">
-                            <NavLink to={"/books"} className="header--ul__li--title">Categories</NavLink>
-                        </li>
-                        <li className="header--ul__li">
-                            <NavLink to={"/recent"} className="header--ul__li--title">Recent</NavLink>
-                        </li>
-                        <li className="header--ul__li">
-                            <NavLink to={"/hello"} className="header--ul__li--title">Books</NavLink>
-                        </li>
-                        <li className="header--ul__li">
-                            <NavLink to={"/about"} className="header--ul__li--title">About Us</NavLink>
-                        </li>
+                      <div className="header--ul__burger " style={{
+                          transition: burger ? "margin-left 3s" : " margin-left 3s",
+                          display:burger ? "block" : ""
+
+                      }}>
+                          <li className="header--ul__burger--li">
+                              <NavLink to={"/books"} className="header--ul__burger--li__title">Categories</NavLink>
+                          </li>
+                          <li className="header--ul__burger--li">
+                              <NavLink to={"/recent"} className="header--ul__burger--li__title">Recent</NavLink>
+                          </li>
+                          <li className="header--ul__burger--li">
+                              <NavLink to={"/hello"} className="header--ul__burger--li__title">Books</NavLink>
+                          </li>
+                          <li className="header--ul__burger--li">
+                              <NavLink to={"/about"} className="header--ul__burger--li__title">About Us</NavLink>
+                          </li>
+                      </div>
 
 
                     </ul>
@@ -34,10 +42,18 @@ const Header = () => {
                             <HiOutlineShoppingBag className="header--icons__basket"/>
                         </Link>
                         <div className="header--icons__burger">
-                            <button className="header--icons__burger--btn">
-                                   <span></span>
-                                   <span></span>
-                                   <span></span>
+                            <button className="header--icons__burger--btn" onClick={()=>{
+                                setBurger(!burger)
+                            }}>
+                                   <span style={{
+                                       transform : burger ? "translateY(0) rotate(45deg)" : ""
+                                   }}></span>
+                                   <span style={{
+                                       display: burger ? "none" : ""
+                                   }}></span>
+                                   <span style={{
+                                       transform : burger ? "translateY(0) rotate(-45deg)" : ""
+                                   }}></span>
 
                             </button>
                         </div>
